43e6e067850953f539a4d35ee0c33bdbde21fe83
20P31A04C3 <84895850+20P31A04C3@users.noreply.github.com>
2022-09-10 21:38:19 +0530

    import java.util.Scanner; class perfect{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,sq;         n=sc.nextInt();         double d=Math.sqrt(n);         if(d-(int)d>0)         {             System.out.println("False");         }         else         {             System.out.println("True");         }     } }

---
+++ perfect_square_root_or_not.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class perfect{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,sq;
+        n=sc.nextInt();
+        double d=Math.sqrt(n);
+        if(d-(int)d>0)
+        {
+            System.out.println("False");
+        }
+        else
+        {
+            System.out.println("True");
+        }
+    }
+}
