f1ec1839ddf2d735214bc8de43d3ba90a65473cb
20P31A04C3 <84895850+20P31A04C3@users.noreply.github.com>
2022-09-15 17:46:36 +0530

    import java.util.Scanner; class pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         for(int i=a;i>=1;i--)         {             for(int j=1;j<=i;j++)             {                 System.out.print((char)(64+i)+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -6,11 +6,11 @@ class pattern
         Scanner sc=new Scanner(System.in);
         int a;
         a=sc.nextInt();
-        for(int i=1;i<=a;i++)
+        for(int i=a;i>=1;i--)
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.print(j+"");
+                System.out.print((char)(64+i)+" ");
             }
             System.out.println();
         }
