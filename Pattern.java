4ae426a3519f4fc19dc053b00e19e628e4ddcbee
20P31A04C3 <84895850+20P31A04C3@users.noreply.github.com>
2022-09-15 17:48:49 +0530

    import java.util.Scanner; class pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         for(int i=1;i<=a;i++)         {             for(int j=1;j<=a;j++)             {                 System.out.print(i+" ");             }             System.out.println();         }     } }

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class pattern
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=1;j<=a;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
