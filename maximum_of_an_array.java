885434802ad7f40bbb997ea2a74508aea2166e21
20P31A04C3 <84895850+20P31A04C3@users.noreply.github.com>
2023-01-28 15:30:04 +0530

    import java.util.Scanner; import java.util.Arrays; class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         Arrays.sort(x);         System.out.println(x[n-1]);
        }
    }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class java
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        Arrays.sort(x);
+        System.out.println(x[n-1]);
+        
+    }
+}
